b1454f1cfdb47eb54f8e42617ae1354a4de5422b
hmasudas <91448643+hmasudas@users.noreply.github.com>
2021-10-14 12:29:06 +0900

    delete commit
    oseti

--- oseti/sample.py
+++
@@ -1,6 +0,0 @@
-import oseti
-
-analyzer = oseti.Analyzer()
-print(analyzer.count_polarity('私はとっても幸せ'))
-print(analyzer.count_polarity('店員の態度は悪かったが、ここのラーメンはやばい'))
-print(analyzer.count_polarity('遅刻したけど楽しかったし嬉しかった。すごく充実した！'))
